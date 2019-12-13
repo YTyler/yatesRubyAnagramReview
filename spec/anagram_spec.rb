6e66c6e67d1e53966b6101ebd822a1294ac12e9d
Tyler Yates <tyates907@gmail.com>
2019-12-13 12:11:36 -0800

    add case sensitivity test

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -8,6 +8,9 @@ describe ('#anagram') do
     expect(test_word.anagram?('tea')).to(eq('These are anagrams'))
   end
 
-
+  it('odd capitalizations still pass anagram test') do
+    test_word = Word.new('eAt')
+    expect(test_word.anagram?('TeA')).to(eq('These are anagrams'))
+  end
 
 end
