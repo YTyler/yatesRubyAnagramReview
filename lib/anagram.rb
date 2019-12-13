738b20bdd680b27770c4884d1d4e66a8358f5e4e
Tyler Yates <tyates907@gmail.com>
2019-12-13 12:12:22 -0800

    check for case insensitivity

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,17 @@
 class Word
   def initialize(word)
     @word = word
-    @value = word.bytes.sum
+    @value = word.downcase.bytes.sum
   end
 
   def anagram?(check)
-    check_value = check.bytes.sum
+    check_value = check.downcase.bytes.sum
     if @value == check_value
       return 'These are anagrams'
     else
       return "These are not anagrams"
+      puts @value
+      puts check_value
     end
   end
 
