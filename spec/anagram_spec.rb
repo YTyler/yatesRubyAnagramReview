40d8412aacbbf2fb63c1355c0bd5fd434e1794f7
Tyler Yates <tyates907@gmail.com>
2019-12-13 11:23:03 -0800

    write anagram test

---
+++ spec/anagram_spec.rb
@@ -0,0 +1,13 @@
+require('rspec')
+require('anagram.rb')
+
+describe ('#anagram') do
+
+  it('test if two words are anagrams') do
+    test_word = Word.new('eat')
+    expect(test_word.anagram?('tea')).to.(eq('These words are anagrams'))
+  end
+
+
+
+end
