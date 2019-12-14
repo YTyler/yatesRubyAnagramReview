2bc7b085b43cd5023f717169bdd6157103a3e15d
Tyler Yates <tyates907@gmail.com>
2019-12-13 16:08:46 -0800

    write multi-word/punctuation/whitespace test

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -22,4 +22,10 @@ describe ('#anagram') do
     test_word = Word.new('unique')
     expect(test_word.anagram?('copy')).to(eq('These are antigrams'))
   end
+
+  it('checks anagram status of multiple words ignoring punctuation and empty space') do
+    test_word = Word.new('I am...Lord Voldemort!')
+    expect(test_word.anagram?('Riddle, Tom Marvolo')).to(eq('These are anagrams'))
+  end
+
 end
